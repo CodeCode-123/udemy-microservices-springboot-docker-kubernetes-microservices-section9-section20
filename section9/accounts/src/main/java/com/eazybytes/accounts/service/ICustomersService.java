ff8f103b4980e041e27b4aa09154b598093d2f94
Shen Tang <adatang23@yahoo.com>
2026-06-01 17:05:53 -0400

    Modified the Gateway server application.yaml to accept service names with lower case, implemented Custom Routing to change paths of URIs using Spring Cloud Gateway, added X-Response-Time to the Response Header, and added CORRELATION_ID to track each request through different microservices

--- section9/accounts/src/main/java/com/eazybytes/accounts/service/ICustomersService.java
+++ section9/accounts/src/main/java/com/eazybytes/accounts/service/ICustomersService.java
@@ -8,5 +8,5 @@ public interface ICustomersService {
      * @param mobileNumber - Input Mobile Number
      * @return Customer based on a given mobileNumber
      */
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
 }
