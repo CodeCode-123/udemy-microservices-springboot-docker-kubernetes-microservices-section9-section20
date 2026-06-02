59a521dcf419621656cba7f3788125fafbdaa6d8
Shen Tang <adatang23@yahoo.com>
2026-06-02 14:08:13 -0400

    Added Retry Pattern to the Gateway and Accounts and configured the application.yaml files

--- section_10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/ResponseTraceFilter.java
+++ section_10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/filters/ResponseTraceFilter.java
@@ -22,8 +22,11 @@ public class ResponseTraceFilter {
           return chain.filter(exchange).then(Mono.fromRunnable(() -> {
               HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
               String correlationId = filterUtility.getCorrelationId(requestHeaders);
-              logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-              exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+              //try to avoid repeated correlation_id due to retries, check if headers does not contain id
+              if (!(exchange.getResponse().getHeaders().containsHeader(FilterUtility.CORRELATION_ID))) {
+                  logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+                  exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+              }
           }));
         };
     }
--- section_10/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ section_10/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -260,6 +260,7 @@ public class LoansController {
     })
     @GetMapping("/contact-info")
     public ResponseEntity<LoansContactInfoDto> getLoansContactInfo() {
+        logger.debug("Invoked Loans contact-info API");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(loansContactInfoDto);
