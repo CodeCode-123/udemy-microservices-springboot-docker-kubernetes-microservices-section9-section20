1ca7090d279e6b50097db6f90d91140fc156e7be
Shen Tang <adatang23@yahoo.com>
2026-06-06 19:13:59 -0400

    Added Role-based authentication and authorization with a KeycloakRoleConverter, registered client & end user inside KeyCloak for Authorization code grant flow, and tested by Postman and KeyCloak

--- section_12/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
+++ section_12/gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/SecurityConfig.java
@@ -3,13 +3,19 @@ package com.eazybytes.gatewayserver.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -23,11 +29,11 @@ public class SecurityConfig {
                 //if the method is GET, permit all without authentication
                         .pathMatchers(HttpMethod.GET).permitAll()
                 //if the URI is /accounts, /cards, /loans, requires authentication, except for the GET methods
-                .pathMatchers("/eazybank/accounts/**").authenticated()
-                .pathMatchers("/eazybank/cards/**").authenticated()
-                .pathMatchers("/eazybank/loans/**").authenticated())
+                .pathMatchers("/eazybank/accounts/**").hasRole("ACCOUNTS")
+                .pathMatchers("/eazybank/cards/**").hasRole("CARDS")
+                .pathMatchers("/eazybank/loans/**").hasRole("LOANS"))
                 .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()));
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
         //csrf protection is required when use frontend browser, otherwise, GET, POST, PUT, PATCH, DELETE
         //methods will fail due to not handle csrf protection
         serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
@@ -39,5 +45,12 @@ public class SecurityConfig {
         //KeyCloak uses Asymmetric algorithm RS256 to sign JWTs
         //the jwt-set-uri is used to get the Public Key
         return NimbusReactiveJwtDecoder.withJwkSetUri(jwtSetUri).build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
