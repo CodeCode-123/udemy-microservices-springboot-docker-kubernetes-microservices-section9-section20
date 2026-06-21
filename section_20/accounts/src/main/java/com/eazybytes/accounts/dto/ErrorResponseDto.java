80b662b4b9691a2f7e035d38c86d066970e2a027
Shen Tang <adatang23@yahoo.com>
2026-06-21 04:01:25 -0400

    Added Spring Boot BOM to manage versions of different microservices centrally

--- section_20/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
+++ section_20/accounts/src/main/java/com/eazybytes/accounts/dto/ErrorResponseDto.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+
 import java.time.LocalDateTime;
 
 @Data
--- section_20/loans/src/main/java/com/eazybytes/loans/dto/ErrorResponseDto.java
+++ section_20/loans/src/main/java/com/eazybytes/loans/dto/ErrorResponseDto.java
@@ -7,31 +7,27 @@ import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-@Data @AllArgsConstructor
+@Data
+@AllArgsConstructor
 @Schema(
-        name = "ErrorResponse",
-        description = "Schema to hold error response information"
+        name="ErrorResponse",
+        description="Schema to hold error response information"
 )
 public class ErrorResponseDto {
-
     @Schema(
-            description = "API path invoked by client"
+            description="API path invoked by client"
     )
-    private  String apiPath;
-
+    private String apiPath;
     @Schema(
-            description = "Error code representing the error happened"
+            description="Error code representing the error happened"
     )
     private HttpStatus errorCode;
-
     @Schema(
-            description = "Error message representing the error happened"
+            description="Error message representing the error happened"
     )
-    private  String errorMessage;
-
+    private String errorMessage;
     @Schema(
-            description = "Time representing when the error happened"
+            description="Time representing when the error happened"
     )
     private LocalDateTime errorTime;
-
 }
